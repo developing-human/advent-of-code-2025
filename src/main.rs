f92b8df1e9de10dfba2dc04849e9561b10e3a500
Developing Human <developinghuman@protonmail.com>
2025-12-02 10:50:23 -0500

    restructure to one binary

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use aoc::problems::*;
+use std::process::exit;
+
+fn main() {
+    let first_arg: String = std::env::args().nth(1).expect("problem number is required");
+    let filename = format!("inputs/{}.txt", first_arg);
+    let input = std::fs::read_to_string(&filename).unwrap_or_else(|_| {
+        eprintln!("file does not exist: {filename}");
+        exit(1);
+    });
+
+    let output = match first_arg.as_str() {
+        "1" => problem1::solve(&input),
+        "2" => problem2::solve(&input),
+        _ => {
+            eprintln!("{first_arg} is not yet implemented");
+            exit(1);
+        }
+    };
+
+    println!("{output:?}");
+}
